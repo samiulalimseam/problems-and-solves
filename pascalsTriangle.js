4368a3eece9b24c15a4acd87893bca9773f87f98
samiul alim <samiulalimseam@gmail.com>
2023-09-09 01:19:26 +0600

    added new pascel issue

---
+++ pascalsTriangle.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    const x = 1
+    const y = 1
+     return Math.pow(x+y,numRows-1)
+    
+};
+
+
+function generatePalindromeArrayWithSum(targetSum) {
+    for (let index = 0; index < targetSum; index++) {
+        let palArray = [1]
+        for (let j = 0; j <=index ; j++) {
+            for (let k = 1; k <= 10; k++) {
+                palArray[j] = k
+                 
+                
+            }
+           
+            
+        } 
+        console.log(palArray);
+    }
+   
+  }
+  
+ generatePalindromeArrayWithSum(generate(5))
+
+ 
+  
+
+  
