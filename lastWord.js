b7ddb672fe93275ae547d09893e72ee7e7e89ccc
samiul alim <samiulalimseam@gmail.com>
2023-10-18 00:05:56 +0600

    added last word length of a string

---
+++ lastWord.js
@@ -0,0 +1,16 @@
+var lengthOfLastWord = function(s) {
+    let length = 0;
+    let i = s.length - 1;
+
+    while (i >= 0 && s[i] === ' ') {
+        i--;
+    }
+
+    while (i >= 0 && s[i] !== ' ') {
+        length++;
+        i--;
+    }
+    return length;
+};
+
+
