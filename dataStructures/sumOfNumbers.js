af1a0d17754a08591f3968e60e7a8cf8dea57d6c
samiulalimseam <samiulalimseam@gmail.com>
2024-01-19 10:41:48 +0600

    performance test for loop and math

--- dataStructures/sumOfNumbers.js
+++ dataStructures/sumOfNumbers.js
@@ -1,4 +1,15 @@
 function sumOfNumbers (numbers) {
     return numbers * (numbers+1)/ 2
 }
-console.log(sumOfNumbers(12))
+
+function sumOfNumbersLoop (nums){
+    let sum = 0
+    for(let i=0; i<=nums; i++) {
+        sum+= i;
+    }
+    return sum;
+} 
+const start = (performance.now() / 1000)
+console.log(sumOfNumbers(9999999))
+const end = (performance.now() / 1000)
+console.log((end - start).toFixed(5) )
